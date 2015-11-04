9232ed69b0076783608bf9b9b2db3a0e6a290536
juliusbacosa <juliusbacosa@gmail.com>
2015-11-04 12:58:54 +0800

    commit

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace AngularJs.Controllers
+{
+    public class Todo
+    {
+        public string action { get; set; }
+        public bool done { get; set; }
+    }
+
+    [EnableCors(origins: "http://angularjs.localhost:8088", headers: "*", methods: "*")]
+    public class TodoController : ApiController
+    {
+        public IEnumerable<Todo> Get()
+        {
+            var todoList = new List<Todo>
+            {
+                new Todo { action="Go Biking" , done= true },
+                new Todo { action="Go Swimming" , done= true },
+                new Todo { action="Go Eating" , done= true },
+                new Todo { action="Go Cooking" , done= true },
+
+            };
+            return todoList.ToList();
+        }
+    }
+}
---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace AngularJs.Controllers
+{
+
+    public class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    
+
+    
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<Person> Get()
+        {
+
+            var people = new List<Person>
+            {
+                new Person { ID=1, Name="Julius Bacosa", Age=32 },
+                new Person { ID=2, Name="Anakin Skywalker", Age=25 },
+                new Person { ID=3, Name="Obiwan Kinobi", Age=28 }
+            };
+
+
+            return people.ToList();
+        }
+
+        
+
+
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
